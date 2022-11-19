63c2bfc6aefa4c88418684154190b31276912b92
Võ Quang Chiến <87252943+yeuxacucodon@users.noreply.github.com>
2022-11-19 20:07:22 +0700

    Create 2.pas

---
+++ Rẽ nhánh/2.pas
@@ -0,0 +1,32 @@
+var
+	a,b,c:real;
+	delta,x1,x2:real;
+begin
+	writeln('Chuong trinh giai phuong trinh bac 2');
+	writeln('--------------------------');
+	write('Nhap a: '); read(a);
+	write('Nhap b: '); read(b);
+	write('Nhap c: '); read(c);
+	delta:=sqr(b)-4*a*c;
+	if delta>0 then
+		begin
+			x1:=(-b + sqrt(delta))/(2 * a);
+			x2:=(-b - sqrt(delta))/(2 * a);
+			writeln('-------------------');
+			writeln('Phuong trinh co 2 nghiem phan biet: ');
+			writeln('x1 = ',x1:0:2);
+			writeln('x2 = ',x2:0:2);
+		end
+	else if delta=0 then
+		begin
+			x1:=-b / (2 * a);
+			writeln('-----------------------');
+			writeln('Phuong trinh co nghiem kep');
+			writeln('x1 = x2 = ',x1:0:2);
+		end
+	else
+		begin
+			writeln('-----------------');
+			writeln('Phuong trinh vo nghiem');
+		end;
+end.
