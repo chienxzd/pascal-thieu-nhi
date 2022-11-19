01096741b562e0079e0e846d7a3ca4dfa9e60149
Võ Quang Chiến <87252943+yeuxacucodon@users.noreply.github.com>
2022-11-19 21:45:29 +0700

    Create 2.pas

---
+++ Vòng lặp/2.pas
@@ -0,0 +1,19 @@
+var
+	a,b:byte;
+begin
+	a:=2;
+	b:=1;
+	while a<10 do
+		begin
+			writeln('Bang cuu chuong ',a);
+			writeln('----------------');
+			while b<=10 do
+				begin
+					writeln(a,' x ',b,' = ',a*b);
+					b:=b+1;
+				end;
+			writeln;
+			b:=1;
+			a:=a+1;
+		end;
+end.
