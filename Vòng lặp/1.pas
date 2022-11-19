1baa80e82caf6a4e7838e1f11d62523cae7becbf
Võ Quang Chiến <87252943+yeuxacucodon@users.noreply.github.com>
2022-11-19 20:53:44 +0700

    Update 1.pas

--- Vòng lặp/1.pas
+++ Vòng lặp/1.pas
@@ -1,1 +1,13 @@
-
+var
+	a,b:byte;
+begin
+	for a:=2 to 9 do
+		begin
+			writeln('Bang cuu chuong ',a);
+			for b:=1 to 10 do
+				begin
+					writeln(a,' x ',b,' = ',a*b);
+				end;
+			writeln;
+		end;
+end.
