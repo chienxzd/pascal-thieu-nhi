acb7feea701d2156eab63a4d82d1477b262e13bb
Võ Quang Chiến <87252943+yeuxacucodon@users.noreply.github.com>
2022-11-19 19:07:59 +0700

    Update 1.pas

--- Rẽ nhánh/1.pas
+++ Rẽ nhánh/1.pas
@@ -1,1 +1,10 @@
-
+var
+	n:integer;
+begin
+	write('Nhap n: '); read(n);
+	writeln('-----------------');
+	if (n mod 2 = 0) then
+		writeln(n,' la so chan')
+	else
+    writeln(n,' la so le');
+end.
