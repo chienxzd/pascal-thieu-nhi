9c4bb0818400acc0285ad6a9d15907ab48489189
Võ Quang Chiến <87252943+yeuxacucodon@users.noreply.github.com>
2022-11-20 14:57:46 +0700

    Create 4.pas

---
+++ Vòng lặp/4.pas
@@ -0,0 +1,10 @@
+var
+	n,i,giaithua:integer;
+begin
+	giaithua:=1;
+	write('Nhap n = '); read(n);
+	writeln('-----------------');
+	for i:=1 to n do
+		giaithua:=giaithua*i;
+	writeln(n,'! = ',giaithua);
+end.
