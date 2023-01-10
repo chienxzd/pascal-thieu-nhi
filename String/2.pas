b5831069c10516b67585204de9f030eae41c7284
yeuxacucodon <chiendien2006@gmail.com>
2023-01-10 21:31:31 +0700

    them bai 2 ae

---
+++ String/2.pas
@@ -0,0 +1,18 @@
+uses crt;
+var
+    dem,i:integer;
+    s:string;
+    c:char;
+begin
+    clrscr;
+    dem:=0;
+    write('Nhap xau: '); readln(s);
+    write('Nhap 1 ky tu trong xau: '); readln(c);
+    for i:=1 to length(s) do
+        begin
+            if s[i] = c then
+                dem:=dem+1;
+        end;
+    writeln(dem);
+    readln;
+end.
